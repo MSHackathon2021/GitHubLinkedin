9d25bbcaf1a3a31e152bf9d9a9cc8df0b00fcbca
Arun Menon <arpn@microsoft.com>
2021-09-27 13:39:30 +0530

    Local host refernce added to redirect uri

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     'linkedinAuth': {
       'clientID': '77zv1fb5adj9a8', // your App ID
       'clientSecret': '57OVVQ3Vj6oYpAKE', // your App Secret
-      'callbackURL': 'https://githublinkedin.azurewebsites.net/auth/linkedin/callback'
+     'callbackURL': 'http://127.0.0.1:2400/auth/linkedin/callback'
     }
   }
